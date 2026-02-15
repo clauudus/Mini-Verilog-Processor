d8ea4f6431c1ece5d274dab7b3263c861d7b71d6
Clau <96309298+clauudus@users.noreply.github.com>
2026-02-15 19:33:22 +0100

    Update Program2.asm
    Correction of infinite loop. Since we don't have conditional jumps we cannot do compares. Unrolling the loop, we avoid this issue.

--- programs/Program2.asm
+++ programs/Program2.asm
@@ -1,21 +1,14 @@
 ; Adds 3 five times -> result = 15
 
-        LDI R1, 3      ; value to add
-        LDI R2, 0      ; acumulator
-        LDI R3, 5      ; compt
+LDI R1, 3
+LDI R2, 0
 
-loop:
-        ADD R2, R2, R1 ; R2 = R2 + R1
-        SUB R3, R3, R4 ; R4 = 0 (assumim R4=0 inicialment)
-        LDI R4, 1
-        SUB R3, R3, R4 ; decrement R3
+ADD R2, R2, R1
+ADD R2, R2, R1
+ADD R2, R2, R1
+ADD R2, R2, R1
+ADD R2, R2, R1
 
-        JMP check
+ST R2, 0x20
+HALT
 
-check:
-        ; Since we don't have a compare loop,
-        ; we do manually the loop
-        JMP loop
-
-        ST R2, 0x20    ; Save result a mem[0x20]
-        HALT
